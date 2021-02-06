12536be24e3128aff9dbeeb3862e7188303d56f7
martin kuracka <m.kuracka@gmail.com>
2021-02-06 16:48:19 +0100

    validation

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,23 @@
 const handleRegister = (req, res, db, bcrypt) => {
 	const { email, name, password } = req.body;
+	const ValidateEmail = (mail) => {			 
+			 if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
+			  {			  	
+			    return (true)
+			  }
+			    res.status(400).json("You have entered an invalid email address!")	           
+	}
+
+	const ValidatePassword = (pass) => {
+			if (password.length > 6) {
+				return (true)
+			} 
+				res.status(400).json('Password must be longer than 6 characters')			 
+	}
+
 	if (!name || !email || !password) {
-		res.status(400).json('all fields must be filled')
-		return
-	} else {
+		return res.status(400).json('all fields must be filled')		
+	} else if (ValidateEmail(true) && ValidatePassword(true)){
 		const hash = bcrypt.hashSync(password); 
 		db.transaction(trx => { 
 			trx.insert({ 
@@ -25,7 +39,7 @@ const handleRegister = (req, res, db, bcrypt) => {
 			}).then(trx.commit) 
 				.catch(trx.rollback)
 		}).catch(err => res.status(400).json('unable to register')); 
-  }
+  } 
 }
 
 module.exports = {
