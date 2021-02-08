5e1f1fef95a6cf2aef945df92f8b146a2cfb09b8
martin kuracka <m.kuracka@gmail.com>
2021-02-08 13:23:01 +0100

    fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = knex({
 app.use(bodyParser.json()); 
 app.use(cors()); 
 app.get('/', (req, res) => {
-	res.send(db.select('*').from('users'));
+	res.send('it is working');
 })
 
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
